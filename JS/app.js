aaab0a4127acd489abd4d3e0a36a7d9f9fbb2f13
emma <emma.ronaldo7@gmail.com>
2021-10-16 18:56:38 -0400

    add play button event listener

--- JS/app.js
+++ JS/app.js
@@ -10,16 +10,26 @@
 
 /*--------------------- Cached Element References ---------------------*/
 
-const sGameBtn = document.querySelector('sgame-button');
-const bridgertonBtn = document.querySelector('bridg-button');
-const lcdpBtn = document.querySelector('lcdp-button');
-const tqgBtn = document.querySelector('tqg-button');
-const playBtn = document.querySelectorO('play-btn');
-const categorizeContainer = document.querySelector('categorize-container');
+
+const h1 = document.querySelector('#heading');
+const sGameBtn = document.querySelector('#sgame-button');
+const bridgertonBtn = document.querySelector('#bridg-button');
+const lcdpBtn = document.querySelector('#lcdp-button');
+const tqgBtn = document.querySelector('#tqg-button');
+const playBtn = document.querySelector('#play-btn');
+const categorizeContainer = document.querySelector('#categorize-container');
+
 
 /*------------------------- Event Listeners ---------------------------*/
 
+playBtn.addEventListener('click', startGame);
 
 
 
 /*----------------------------- Functions -----------------------------*/
+
+function startGame() {
+  playBtn.style.display = 'none';
+	h1.style.display = 'none';
+	categorizeContainer.classList.remove('hide');
+}

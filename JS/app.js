731ebbb6451982059277aa5bc697b6783b90cca0
emma <emma.ronaldo7@gmail.com>
2021-10-16 16:55:02 -0400

    add cached elements

--- JS/app.js
+++ JS/app.js
@@ -10,8 +10,12 @@
 
 /*--------------------- Cached Element References ---------------------*/
 
-
-
+const sGameBtn = document.querySelector('sgame-button');
+const bridgertonBtn = document.querySelector('bridg-button');
+const lcdpBtn = document.querySelector('lcdp-button');
+const tqgBtn = document.querySelector('tqg-button');
+const playBtn = document.querySelectorO('play-btn');
+const categorizeContainer = document.querySelector('categorize-container');
 
 /*------------------------- Event Listeners ---------------------------*/
 
